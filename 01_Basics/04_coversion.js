7366f18f102a22a7b82b022854916ff7c9fbfbc5
Raj7405 <137071107+Raj7405@users.noreply.github.com>
2024-01-05 10:04:05 +0000

    coversion

---
+++ 01_Basics/04_coversion.js
@@ -0,0 +1,27 @@
+let score="raj"
+
+//both are way to check types
+//console.log(typeof score)
+//console.log(typeof(score))
+
+//type coversion
+//let valueInNumber = Number(score)
+
+//console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN
+// true => 1, false => 0;
+// 1 => true,  0 => false
+// "" => false
+// "Raj" => true
+
+let a ;
+let b = Boolean(a)
+
+console.log(a);
+console.log(b);
+console.log(typeof b);
+
+//null => False
