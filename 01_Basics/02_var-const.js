822c622606d8b353a3390fa6b7938705bc549bed
Raj7405 <137071107+Raj7405@users.noreply.github.com>
2024-01-05 09:36:59 +0000

    change in file name

---
+++ 01_Basics/02_var-const.js
@@ -0,0 +1,17 @@
+//to defined constant value
+const accountId=2001
+
+//to prevent block scoping issue in Js
+let accountEmail="raj@gmail.com"
+
+//normal way of declaring Varible
+var accountName="Rajendra"
+
+//we can declare varible without keyword
+accountCity="Mumbai"
+
+
+//varible without value will be by default "undefined"
+let accountState;
+
+console.table([accountEmail,accountId,accountName,accountCity,accountState])
