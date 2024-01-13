b1989ff194448a5c8fc6ab78b44b1ef9e0b1d17c
Raj7405 <137071107+Raj7405@users.noreply.github.com>
2024-01-13 10:00:10 +0000

    call of fuction

---
+++ 03_Basics_function/02_scope.js
@@ -0,0 +1,12 @@
+//two to declare function 
+
+myFunc1(5);
+function myFunc1(num){
+    return num + 1
+}
+
+//it will give error for not declarin varible of function before using it
+myFunc2(5)
+const myFunc2 = function(num){
+    return num + 2
+}
