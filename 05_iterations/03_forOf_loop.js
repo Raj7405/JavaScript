3a5e3a0b1b49e7500b04bf3f1fd2d02d6e02543f
Raj7405 <137071107+Raj7405@users.noreply.github.com>
2024-01-18 12:25:15 +0000

    for of loop and basic of map

---
+++ 05_iterations/03_forOf_loop.js
@@ -0,0 +1,49 @@
+// for i of arr1
+//here i is not index value it is value itself
+// ["", "", ""]
+// [{}, {}, {}]
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const num of arr) {
+    //console.log(num);
+}
+
+const greetings = "Hello world!"
+for (const greet of greetings) {
+    //console.log(`Each char is ${greet}`)
+}
+
+// Maps
+const map1 = new Map([
+    [1,"raj"],
+    [2,"sonu"],
+    [3,"raju"],
+])
+//console.log(map1);
+for( const [key,value] of map1){
+    console.log(key, ':-',value);
+}
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+
+//console.log(map);
+
+for (const [key, value] of map) {
+    // console.log(key, ':-', value);
+}
+
+const myObject = {
+    game1: 'NFS',
+    game2: 'Spiderman'
+}
+
+// for (const [key, value] of myObject) {
+//     console.log(key, ':-', value);
+    
+// }
