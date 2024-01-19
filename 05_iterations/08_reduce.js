f4bfda6941dcda2cc1f6a16c90f29de973e65d9c
Raj7405 <137071107+Raj7405@users.noreply.github.com>
2024-01-19 11:27:16 +0000

    reduce() method

---
+++ 05_iterations/08_reduce.js
@@ -0,0 +1,27 @@
+
+const myNums = [1,2,3]
+
+const myTOtal = myNums.reduce((acc,currVal) => (acc + currVal),0)
+console.log(myTOtal);
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const totalCost = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+console.log(totalCost);
