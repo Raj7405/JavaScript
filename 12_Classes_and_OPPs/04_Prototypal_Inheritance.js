d640daf784618482ad194b39c3dc351875d572d1
Rajendra Mohan Chaudhari <137071107+Raj7405@users.noreply.github.com>
2024-03-05 10:24:04 +0000

    prototypal inheritance(__proto__ & .setPrototype)

---
+++ 12_Classes_and_OPPs/04_Prototypal_Inheritance.js
@@ -0,0 +1,25 @@
+const User = {
+    name: "raj",
+    email: "raj@code.com"
+}
+
+const Teacher = {
+    makeVideo: true
+}
+
+const TeachingSupport = {
+    isAvailable: false
+}
+
+const TASupport = {
+    makeAssignment: 'Js assignment',
+    fullTime: true,
+    __proto__: TeachingSupport
+}
+
+Teacher.__proto__= User;
+Teacher.name = "vijay"
+console.log(Teacher.name);
+
+//modern syntax
+Object.setPrototype(TeachingSupport, Teacher)
