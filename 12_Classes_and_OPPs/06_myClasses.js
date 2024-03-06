14010ee26babebb31afe117d2660b7b643a70f5a
Rajendra Mohan Chaudhari <137071107+Raj7405@users.noreply.github.com>
2024-03-06 09:19:18 +0000

    added behind the scene

--- 12_Classes_and_OPPs/06_myClasses.js
+++ 12_Classes_and_OPPs/06_myClasses.js
@@ -17,3 +17,23 @@ class User {
 const student = new User("raj", "raj@g.com", 123)
 console.log(student.encrptPassword());
 console.log(student.changeUsername())
+
+//behind the scene
+
+function User2(username, email, password){
+    this.username = username;
+    this.email = email;
+    this.password = password;
+}
+
+User2.prototype.encrptPassword_2 = function(){
+    return `${this.password * Math.floor(Math.random() * 10 + 1)}`
+}
+
+User2.prototype.changeUsername_2 = function(){
+    return `${this.username.toUpperCase()}`
+}
+
+const studentTwo = new User2("Sonu", "sonu@Meta.com", 420)
+console.log(studentTwo.encrptPassword_2())
+console.log(studentTwo.changeUsername_2())
