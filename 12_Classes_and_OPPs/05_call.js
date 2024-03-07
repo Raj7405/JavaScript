bb9cd561b524b9013950fc80f1c465bd321dd334
Rajendra Mohan Chaudhari <137071107+Raj7405@users.noreply.github.com>
2024-03-07 09:21:44 +0000

    function borrowing using call()

--- 12_Classes_and_OPPs/05_call.js
+++ 12_Classes_and_OPPs/05_call.js
@@ -12,3 +12,32 @@ function createUser(userName, email, password){
 const chai = new createUser("Raj", "raj@google.com", "123")
 
 console.log(chai);
+
+//object
+function printFullName(){
+    console.log(this.firstName+" "+this.lastName);
+}
+
+function printFullNameTwo(hometown, state){
+    console.log(this.firstName+" "+this.lastName+" from "+hometown+" in "+state);
+}
+
+const user1 = {
+    firstName: "raj",
+    lastName: "Chaudhari"
+}
+
+const user2 = {
+    firstName: "sammed",
+    lastName: "patil"
+}
+
+//this is called function borrwing using call()
+printFullNameTwo.call(user1,"kardh", "Maharastra" );
+printFullNameTwo.call(user2, "sangli", "Maharastra");
+
+
+//bind() function to bind printFullName() with user1
+let func = printFullName.bind(user1);
+
+setTimeout(func,1000)
