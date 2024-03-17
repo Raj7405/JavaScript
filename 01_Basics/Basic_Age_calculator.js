debac822424cd6ff58b4d09fb6877450d0c9bcba
Rajendra Mohan Chaudhari <137071107+Raj7405@users.noreply.github.com>
2024-03-17 09:32:57 +0000

    age calculator updateed

--- 01_Basics/Basic_Age_calculator.js
+++ 01_Basics/Basic_Age_calculator.js
@@ -1,9 +1,24 @@
 let currentDate = Date.now()
 
-let myBob = new Date('02 december 2004')
+let myBob = new Date('02 december 2001')
 // console.log(myBob.getTime());
 // console.log(currentDate);
 
 let myAge = Math.floor((currentDate - myBob)/31557600000)
 
 console.log(`${myAge} years`);
+
+function calculateAge(bob){
+    // let currentDate = new Date("03-17-2024")
+    let currentDate = Date.now()
+    let age = currentDate - bob
+    console.log(`
+    ${Math.floor(age/(1000*60*60*24*365))} year
+    ${Math.floor(age/(1000*60*60*24))} days 
+    ${Math.floor(age/(1000*60*60))} hours 
+    ${Math.floor(age/(1000*60))} minites
+    ${Math.floor(age/(1000))}  seconds
+    `)
+}
+
+calculateAge(myBob)
