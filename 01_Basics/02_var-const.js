8e6c40de0e5ba30efb624901d124ea30897c8298
Rajendra Mohan Chaudhari <137071107+Raj7405@users.noreply.github.com>
2024-03-17 10:03:59 +0000

    added comment for "const" related

--- 01_Basics/02_var-const.js
+++ 01_Basics/02_var-const.js
@@ -1,5 +1,11 @@
 //to defined constant value
 const accountId=2001
+/**
+ In JavaScript, the const keyword does not make a variable immutable (unchangeable), 
+ but rather it prevents reassignment of the variable itself. 
+ This means that you cannot assign a new value to a variable declared with const,
+ but you can modify the content of a variable that holds a mutable data type, such as an array or object
+ */
 
 //to prevent block scoping issue in Js
 let accountEmail="raj@gmail.com"
