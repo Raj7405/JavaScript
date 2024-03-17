d55ef173486e5c794b0999e62c220fe553b3d900
Rajendra Mohan Chaudhari <137071107+Raj7405@users.noreply.github.com>
2024-03-17 12:21:33 +0000

    added some code for better understanding

--- 03_Basics_function/04_IIFE.js
+++ 03_Basics_function/04_IIFE.js
@@ -10,3 +10,41 @@
 ((place) => {
     console.log(`chal na bhai vudar ${place} me jara!!`)
 })("garden")
+
+//1.Encapsulation and Scope Isolation:IFEs help in creating a private scope for variables and functions, preventing them from polluting the global scope. This is particularly useful in large codebases or when working with multiple scripts to avoid naming conflicts.
+
+// (function(value) {
+//     // Private variables and functions
+//     var counter = value;
+  
+//     function incrementCounter() {
+//       counter++;
+//     }
+  
+//     // Expose only what's needed
+//     window.Map = {
+//       increment: incrementCounter
+//     };
+//   })();
+  
+//   myApp.increment();
+
+//2.Module Pattern: IIFEs are often used in conjunction with the module pattern to create modules with private and public members. This pattern is useful for organizing code into smaller, reusable components.
+
+  var myModule = (function() {
+    var privateVar = 'This is private';
+  
+    function privateFunction() {
+      console.log(privateVar);
+    }
+  
+    return {
+      publicFunction: function() {
+        privateFunction();
+      }
+    };
+  })();
+  
+  myModule.publicFunction();
+  
+  
